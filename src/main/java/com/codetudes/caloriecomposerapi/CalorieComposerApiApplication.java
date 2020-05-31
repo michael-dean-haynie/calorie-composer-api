2dc194b59396f9d8b3e19ec1d2661c7096ce5cfc
Michael Dean Haynie <michael.dean.haynie@gmail.com>
2020-05-31 15:08:50 -0400

    Initial commit

---
+++ src/main/java/com/codetudes/caloriecomposerapi/CalorieComposerApiApplication.java
@@ -0,0 +1,13 @@
+package com.codetudes.caloriecomposerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieComposerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieComposerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codetudes/caloriecomposerapi/CalorieComposerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codetudes.caloriecomposerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieComposerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
