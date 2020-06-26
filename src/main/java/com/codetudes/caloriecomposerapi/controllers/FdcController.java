f238406e8881cf5d1c1e2de562e77500f73d6102
Michael Dean Haynie <michael.dean.haynie@gmail.com>
2020-06-26 13:17:35 -0400

    Add get endpoint for single fdc foodd

--- src/main/java/com/codetudes/caloriecomposerapi/controllers/FdcController.java
+++ src/main/java/com/codetudes/caloriecomposerapi/controllers/FdcController.java
@@ -1,22 +1,37 @@
 package com.codetudes.caloriecomposerapi.controllers;
 
+import com.codetudes.caloriecomposerapi.contracts.FoodDTO;
 import com.codetudes.caloriecomposerapi.contracts.SearchResultDTO;
 import com.codetudes.caloriecomposerapi.services.FdcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/fdc")
 public class FdcController {
+
+    static final Logger LOG = LoggerFactory.getLogger(FdcController.class);
 
     @Autowired
     private FdcService fdcService;
 
     @GetMapping("/search")
     SearchResultDTO search(@RequestParam String query) {
+        LOG.info("Received request to search fdc with query: {}", query);
+        SearchResultDTO searchResultDTO = fdcService.search(query);
+        LOG.info("Successfully searched fdc with query: {}", query);
         return fdcService.search(query);
     }
+
+    @GetMapping("/food/{fdcId}")
+    FoodDTO read(@PathVariable String fdcId) {
+        LOG.info("Received request to read fdc with fdcId {}", fdcId);
+        FoodDTO foodDTO = fdcService.getFoodByFdcId(fdcId);
+        LOG.info("Successfully read fdc food with fdcId {}", fdcId);
+        return foodDTO;
+    }
+
+
 }
