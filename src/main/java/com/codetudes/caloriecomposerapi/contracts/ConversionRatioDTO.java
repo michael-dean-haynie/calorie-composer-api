4ede6875a75760daa691f75b9dab24a05ffb78dc
Michael Dean Haynie <michael.dean.haynie@gmail.com>
2021-01-16 15:15:09 -0500

    remove old validation, increase db column size as needed

--- src/main/java/com/codetudes/caloriecomposerapi/contracts/ConversionRatioDTO.java
+++ src/main/java/com/codetudes/caloriecomposerapi/contracts/ConversionRatioDTO.java
@@ -3,29 +3,23 @@ package com.codetudes.caloriecomposerapi.contracts;
 import lombok.Data;
 
 import javax.validation.Valid;
-import javax.validation.constraints.DecimalMax;
-import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Data
 public class ConversionRatioDTO {
     private Long id;
 
-    @DecimalMax("999.99")
     private BigDecimal amountA;
 
     @Valid
     private UnitDTO unitA;
 
-    @Size(max=50)
     private String freeFormValueA;
 
-    @DecimalMax("999.99")
     private BigDecimal amountB;
 
     @Valid
     private UnitDTO unitB;
 
-    @Size(max=50)
     private String freeFormValueB;
 }
--- src/main/java/com/codetudes/caloriecomposerapi/contracts/NutrientDTO.java
+++ src/main/java/com/codetudes/caloriecomposerapi/contracts/NutrientDTO.java
@@ -3,20 +3,16 @@ package com.codetudes.caloriecomposerapi.contracts;
 import lombok.Data;
 
 import javax.validation.Valid;
-import javax.validation.constraints.DecimalMax;
-import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Data
 public class NutrientDTO {
     private Long id;
 
-    @Size(max=45)
     private String name;
 
     @Valid
     private UnitDTO unit;
 
-    @DecimalMax("999.99")
     private BigDecimal amount;
 }
